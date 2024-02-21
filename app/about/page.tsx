11611f1b73a708f3ef755b837f55886e9f88a09e
Ryan Huddleston <wryanhuddleston@gmail.com>
2024-02-21 12:20:25 +0900

    redesigned navbar

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        I created this blog to share information about development trends I am
+        interested in and personal life updates
+      </p>
+    </main>
+  );
+}
