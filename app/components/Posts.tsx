f6e549a46d14d9e3d5448a9b725a63a23eccff9e
Ryan Huddleston <wryanhuddleston@gmail.com>
2024-02-29 13:04:17 +0900

    redesigned blog posting list on main page

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -6,13 +6,79 @@ export default function Posts() {
   const posts = getSortedPostsData();
 
   return (
-    <section className="mt-6 mx-auto max-w-2xl">
+    <div className="bg-white py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto flex max-w-2xl flex-col items-end justify-between gap-16 lg:mx-0 lg:max-w-none lg:flex-row">
+          <div className="w-full lg:max-w-lg lg:flex-auto">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Ryan Huddleston&apos;s Blog
+            </h2>
+            <p className="mt-6 text-xl leading-8 text-gray-600">
+              Hello and Welcome to my blog.
+            </p>
+            <img
+              src="/images/ryan_profile.PNG"
+              alt="Ryan Huddleston"
+              className="mt-16 aspect-[6/5] w-full rounded-2xl bg-gray-50 object-cover lg:aspect-auto lg:h-[34.5rem]"
+            />
+          </div>
+          <div className="w-full lg:max-w-xl lg:flex-auto">
+            <h3 className="sr-only">Job openings</h3>
+            <ul className="-my-8 divide-y divide-gray-100">
+              {posts.map((post) => (
+                <li key={post.id} className="py-8">
+                  <dl className="relative flex flex-wrap gap-x-3">
+                    <dt className="sr-only">Role</dt>
+                    <dd className="w-full flex-none text-lg font-semibold tracking-tight text-gray-900">
+                      <a href={`/posts/${post.id}`}>
+                        {post.title}
+                        <span className="absolute inset-0" aria-hidden="true" />
+                      </a>
+                    </dd>
+                    <dt className="sr-only">Description</dt>
+                    <dd className="mt-2 w-full flex-none text-base leading-7 text-gray-600">
+                      {post.title}
+                    </dd>
+                    <dt className="sr-only">Salary</dt>
+                    <dd className="mt-4 text-base font-semibold leading-7 text-gray-900">
+                      {post.date}
+                    </dd>
+                    <dt className="sr-only">Location</dt>
+                    <dd className="mt-4 flex items-center gap-x-3 text-base leading-7 text-gray-500">
+                      <svg
+                        viewBox="0 0 2 2"
+                        className="h-0.5 w-0.5 flex-none fill-gray-300"
+                        aria-hidden="true"
+                      >
+                        <circle cx={1} cy={1} r={1} />
+                      </svg>
+                    </dd>
+                  </dl>
+                </li>
+              ))}
+            </ul>
+            <div className="mt-8 flex border-t border-gray-100 pt-8">
+              <a
+                href="#"
+                className="text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+              >
+                View all posts <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+{
+  /* <section className="mt-6 mx-auto max-w-2xl">
       <h2 className="text-4xl font-bold dark:text-white/90">Blog</h2>
       <ul className="w-full">
         {posts.map((post) => (
           <ListItem key={post.id} post={post} />
         ))}
       </ul>
-    </section>
-  );
+    </section> */
 }
